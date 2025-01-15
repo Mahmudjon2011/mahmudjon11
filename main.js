7acf7fb9183cc4f8504ccf79d5bbf7a073bf220e
Mahmudjon2011 <dildooraxajayeva@gmail.com>
2025-01-15 05:37:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const savol = alert("Hozir sizda random son chiqadi")
+const randomRaqam = Math.round(Math.random() * 100);
+const bot = confirm("Bolmoqchimisiz")
+// console.log(randomRaqam);
+alert (` randomdan chiqqan son :  ${randomRaqam }`)
+
+
+
+
+
+
+
